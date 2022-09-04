cf96b4f279dc7f5e28251be7459337bcbfa866a7
hrrishstaa11 <112813310+hrrishstaa11@users.noreply.github.com>
2022-09-04 21:19:39 +0530

    first commit
    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'balck':'#000000',
+      'primary':'#8585ad',
+      'secondary':'#e0e0eb',
+      'red':'#ff4d4d',
+      'green':'#79d2a6',
+      'blue':'#8585ad',
+      'dark':'#3d3d5c'
+    },
+    productContainer:{
+        'bg':'balck'
+    },
+  },
+  plugins: [],
+  
+}
+
+
+// 'primary':'#b300b3',
